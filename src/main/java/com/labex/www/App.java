6658408289d90695aeb3705883c174ea52d91250
liuwenjie <lwjydcg@163.com>
2019-06-03 15:51:15 +0800

    second

---
+++ src/main/java/com/labex/www/App.java
@@ -0,0 +1,14 @@
+package com.labex.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	Util.printMessage("Hello World! Add New");
+    }
+}
---
+++ src/main/java/com/labex/www/Util.java
@@ -0,0 +1,8 @@
+package com.labex.www;
+
+public class Util
+{
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+}
